0a1f246bded108ec883cd7726ff7dc9860477ba3
Olyn <carolyn.ardjo@gmail.com>
2025-05-03 22:54:24 +0700

    Updated code

---
+++ app/components/Footer.tsx
@@ -0,0 +1,23 @@
+import { assets } from '@/public/assets/assets'
+import Image from 'next/image'
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='mt-20'>
+        <div className='text-center'>
+            <Image src={assets.logo} alt='logo' className='w-36 mx-auto mb-2'/>
+        </div>
+        <div className='text-center sm:flex items-center justify-between border-t border-gray-400 mx-[10%] mt-12 py-6'>
+            <p>© 2025 William Mark. All rights reserved.</p>
+            <ul className='flex items-center gap-10 justify-center mt-4 sm:mt-0'>
+                <li><a target='_blank' href='https://github.com/olynsn15'>GitHub</a></li>
+                <li><a target='_blank' href='instagram.com/olyn.sna'>Instagram</a></li>
+                <li><a target='_blank' href='https://www.linkedin.com/in/carolyn-santana15/'>LinkedIn</a></li>
+            </ul>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,8 +1,10 @@
 import { assets } from '@/public/assets/assets'
 import Image from 'next/image'
-import React, { useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
 const Navbar = () => {
+
+    const [isScroll, setIsScroll] = useState(false);
 
     const sideMenuRef = useRef<HTMLUListElement | null>(null);
 
@@ -18,18 +20,29 @@ const Navbar = () => {
         }
     }
 
+    useEffect(() => {
+        window.addEventListener('scroll', () => {
+            if(scrollY > 50){
+                setIsScroll(true)
+            }
+            else{
+                setIsScroll(false)
+            }
+        })
+    }, [])
+
 
   return (
     <>
     <div className='fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%]'>
         <Image src={assets.header_bg_color} alt='background' className='w-full'/>
     </div>
-    <nav className='w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50'>
+    <nav className={`w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50 ${isScroll ? 'bg-white/50 backdrop-blur-lg shadow-sm' : ''}`}>
         <a href='#top'>
             <Image src={assets.logo} alt={'logo'} className='w-28 cursor-pointer mr-14'/>
         </a>
 
-        <ul className='hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 bg-white shadow-sm bg-opacity-50'>
+        <ul className={`hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 ${isScroll ? '' : 'bg-white/50 shadow-sm'}`}>
             <li><a className='font-ovo' href='#top'>Home</a></li>
             <li><a className='font-ovo' href='#about'>About</a></li>
             <li><a className='font-ovo' href='#services'>Services</a></li>
