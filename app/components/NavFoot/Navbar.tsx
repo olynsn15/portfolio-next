657fbc04ec597e8bc49f83cdd22b599a61f03f4d
Olyn <carolyn.ardjo@gmail.com>
2025-05-17 23:35:30 +0700

    Update delete link

--- app/components/NavFoot/Navbar.tsx
+++ app/components/NavFoot/Navbar.tsx
@@ -1,7 +1,6 @@
 "use client"
 import { assets } from '@/public/assets/assets'
 import Image from 'next/image'
-import Link from 'next/link';
 import React, { useEffect, useRef, useState } from 'react'
 
 const Navbar = () => {
