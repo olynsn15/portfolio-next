9616e078718b12c6ead4180b04d6a3ca26d63dd1
Olyn <carolyn.ardjo@gmail.com>
2026-02-24 16:50:00 +0700

    update portfolio

--- app/components/Experiences.tsx
+++ app/components/Experiences.tsx
@@ -1,45 +1,93 @@
-'use client'
-import { assets, experienceData } from '@/public/assets/assets'
-import Image from 'next/image'
-import React from 'react'
+"use client";
+import { assets, experienceData } from "@/public/assets/assets";
+import Image from "next/image";
+import React from "react";
 import { motion } from "motion/react";
 
 const Experiences = () => {
   return (
-    <motion.div id='experiences' className='w-full px-[10%] py-25 scroll-mt-20' initial={{opacity: 0}} whileInView={{opacity: 1}} transition={{duration: 1}}>
-        <motion.h4 className='text-center mb-2 text-lg font-syne text-purple dark:text-darker-purple font-medium' initial={{y: -20, opacity: 0}} whileInView={{y: 0, opacity: 1}} transition={{duration: 0.5, delay: 0.5}}>
-            ✦ Experiences ✦
-        </motion.h4>
-        <motion.h2 className='text-center text-5xl font-syne font-medium text-dark-purple dark:text-light-purple' initial={{opacity: 0, y: -20}} whileInView={{opacity: 1, y: 0}} transition={{duration: 0.5, delay: 0.5}}>
-            Roles & Activities
-        </motion.h2>
-        <motion.p className='text-center max-w-3xl mx-auto mt-5 mb-12 font-lexend-deca' initial={{opacity: 0}} whileInView={{opacity: 1}} transition={{duration: 0.5, delay: 0.7}}>
-            A comprehensive glimpse into the various responsibilities and exciting ventures I have undertaken
-        </motion.p>
+    <motion.div
+      id="experiences"
+      className="w-full px-[10%] py-25 scroll-mt-20"
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 1 }}
+    >
+      <motion.h4
+        className="text-center mb-2 text-lg font-syne text-purple dark:text-darker-purple font-medium"
+        initial={{ y: -20, opacity: 0 }}
+        whileInView={{ y: 0, opacity: 1 }}
+        transition={{ duration: 0.5, delay: 0.5 }}
+      >
+        ✦ Experiences ✦
+      </motion.h4>
+      <motion.h2
+        className="text-center text-5xl font-syne font-medium text-dark-purple dark:text-light-purple"
+        initial={{ opacity: 0, y: -20 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5, delay: 0.5 }}
+      >
+        Roles & Activities
+      </motion.h2>
+      <motion.p
+        className="text-center max-w-3xl mx-auto mt-5 mb-12 font-lexend-deca"
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 0.5, delay: 0.7 }}
+      >
+        A comprehensive glimpse into the various responsibilities and exciting
+        ventures I have undertaken
+      </motion.p>
 
-        <motion.div className='grid grid-auto-fit-experience gap-6 my-10' initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} transition={{ duration: 0.6, delay: 0.9 }}>
-          {experienceData.map(({ icon, title, subtitle}, index) => (
-            <motion.div whileHover={{ scale: 1.05 }} key={index} className='border border-gray-400 rounded-lg px-8 py-8 hover-shadow cursor-pointer hover:bg-light-hover hover:-translate-y-1 duration-500 dark:hover:bg-dark-hover dark:hover:shadow-white items-center'>
-              <div className='flex items-center gap-4'>
-                <Image src={icon} alt='service icon' className='w-15 h-auto rounded-xl'/>
-                <div>
-                  <h3 className='text-lg text-black dark:text-white leading-tight font-lexend-deca font-medium'>{title}</h3>
-                  <h3 className='text-sm text-gray-600 dark:text-white font-lexend-deca'>{subtitle}</h3>
-                </div>
+      <motion.div
+        className="grid grid-auto-fit-experience gap-6 my-10"
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 0.6, delay: 0.9 }}
+      >
+        {experienceData.map(({ icon, title, subtitle }, index) => (
+          <motion.div
+            whileHover={{ scale: 1.05 }}
+            key={index}
+            className="border border-gray-400 rounded-lg px-8 py-8 hover-shadow cursor-pointer hover:bg-light-hover hover:-translate-y-1 duration-500 dark:hover:bg-dark-hover dark:hover:shadow-white "
+          >
+            <div className="flex items-center gap-4">
+              <Image
+                src={icon}
+                alt="service icon"
+                className="w-15 h-auto rounded-xl"
+              />
+              <div>
+                <h3 className="text-lg text-black dark:text-white leading-tight font-lexend-deca font-medium">
+                  {title}
+                </h3>
+                <h3 className="text-sm text-gray-600 dark:text-white font-lexend-deca">
+                  {subtitle}
+                </h3>
               </div>
-            </motion.div>
-          ))}
-        </motion.div>
+            </div>
+          </motion.div>
+        ))}
+      </motion.div>
 
-        <motion.div initial={{y: 30, opacity: 0}} whileInView={{y: 0, opacity: 1}} transition={{duration: 0.6, delay: 1.2}} className='flex justify-center mt-10 font-lexend-deca'>
-          <a target='_blank' href='/CV-Olyn.pdf' rel="noopener noreferrer" className='flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full mx-4 font-ovo text-white dark:border-white/50 bg-black w-fit dark:bg-transparent'>
-            Check out my CV!
-            <Image src={assets.right_arrow_white} alt='arrow' className='w-5'/>
-          </a>
-        </motion.div>
-
+      <motion.div
+        initial={{ y: 30, opacity: 0 }}
+        whileInView={{ y: 0, opacity: 1 }}
+        transition={{ duration: 0.6, delay: 1.2 }}
+        className="flex justify-center mt-10 font-lexend-deca"
+      >
+        <a
+          target="_blank"
+          href="/CarolynSantana_CV_Latest.pdf"
+          rel="noopener noreferrer"
+          className="flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full mx-4 font-ovo text-white dark:border-white/50 bg-black w-fit dark:bg-transparent"
+        >
+          Check out my CV!
+          <Image src={assets.right_arrow_white} alt="arrow" className="w-5" />
+        </a>
+      </motion.div>
     </motion.div>
-  )
-}
+  );
+};
 
-export default Experiences
+export default Experiences;
--- public/assets/assets.js
+++ public/assets/assets.js
@@ -1,194 +1,230 @@
-import menu_black from './menu-black.png';
-import menu_white from './menu-white.png';
-import close_black from './close-black.png';
-import close_white from './close-white.png';
-import right_arrow_white from './right-arrow-white.png';
-import person from './Person.png';
-import profile from './profile.png';
-import design from './design.jpeg';
-import codes from './codes.jpeg';
-import study from './study.jpeg';
-import C from './C.svg';
-import python from './Python.svg';
-import dart from './Dart.svg';
-import flutter from './flutter-original.svg';
-import kotlin from './Kotlin.svg';
-import swift from './swift-original.svg';
-import mysql from './mysql-original.svg';
-import sqlite from './file-type-sqlite.svg'
-import html from './html5-original.svg';
-import css from './css3-original.svg';
-import js from './javascript-original.svg';
-import react from './react-original.svg';
-import typesecript from './typescript-original.svg';
-import tailwind from './tailwindcss-original.svg';
-import next from './nextjs-original.svg';
-import sasc from './sasc.jpeg';
-import binus from './binus.jpeg';
-import tfisc from './tfisc.png';
-import ada from './ada.jpeg';
-import youthmatters from './youthmatters.png';
-import aidbuddy from './aidbuddy.png';
-import tapesync from './tapesync.png';
-import picverse from './picverse.png';
-import dojomovies from './dojomovies.png';
-import github from './github.svg';
-import github_white from './github_white.svg';
-import linkedin_purple from './linkedin_purple.svg';
-import github_purple from './github_purple.svg';
-import instagram_purple from './instagram_purple.svg';
-import linkedin_lp from './linkedin_lp.svg';
-import github_lp from './github_lp.svg';
-import instagram_lp from './instagram_lp.svg';
+import menu_black from "./menu-black.png";
+import menu_white from "./menu-white.png";
+import close_black from "./close-black.png";
+import close_white from "./close-white.png";
+import right_arrow_white from "./right-arrow-white.png";
+import person from "./Person.png";
+import profile from "./profile.png";
+import design from "./design.jpeg";
+import codes from "./codes.jpeg";
+import study from "./study.jpeg";
+import C from "./C.svg";
+import python from "./Python.svg";
+import dart from "./Dart.svg";
+import flutter from "./flutter-original.svg";
+import kotlin from "./Kotlin.svg";
+import swift from "./swift-original.svg";
+import mysql from "./mysql-original.svg";
+import sqlite from "./file-type-sqlite.svg";
+import html from "./html5-original.svg";
+import css from "./css3-original.svg";
+import js from "./javascript-original.svg";
+import react from "./react-original.svg";
+import typesecript from "./typescript-original.svg";
+import tailwind from "./tailwindcss-original.svg";
+import next from "./nextjs-original.svg";
+import figma from "./figma.png";
+import firebase from "./firebase.svg";
+import sasc from "./sasc.jpeg";
+import binus from "./binus.jpeg";
+import tfisc from "./tfisc.png";
+import ada from "./ada.jpeg";
+import youthmatters from "./youthmatters.png";
+import aidbuddy from "./aidbuddy.png";
+import tapesync from "./tapesync.png";
+import picverse from "./picverse.png";
+import dojomovies from "./dojomovies.png";
+import thalafish from "./thalafish.png";
+import planaway from "./planaway.png";
+import github from "./github.svg";
+import github_white from "./github_white.svg";
+import linkedin_purple from "./linkedin_purple.svg";
+import github_purple from "./github_purple.svg";
+import instagram_purple from "./instagram_purple.svg";
+import linkedin_lp from "./linkedin_lp.svg";
+import github_lp from "./github_lp.svg";
+import instagram_lp from "./instagram_lp.svg";
 
 export const assets = {
-    menu_black,
-    menu_white,
-    close_black,
-    close_white,
-    right_arrow_white,
-    person,
-    profile,
-    design,
-    codes,
-    study,
-    C,
-    python,
-    dart,
-    flutter, 
-    kotlin,
-    swift,
-    mysql,
-    sqlite,
-    html,
-    css,
-    js,
-    react,
-    typesecript,
-    tailwind,
-    next,
-    sasc,
-    binus,
-    tfisc,
-    ada,
-    youthmatters,
-    aidbuddy,
-    tapesync,
-    picverse,
-    dojomovies,
-    github,
-    github_white,
-    github_purple,
-    linkedin_purple,
-    instagram_purple,
-    github_lp,
-    linkedin_lp,
-    instagram_lp,
+  menu_black,
+  menu_white,
+  close_black,
+  close_white,
+  right_arrow_white,
+  person,
+  profile,
+  design,
+  codes,
+  study,
+  C,
+  python,
+  dart,
+  flutter,
+  kotlin,
+  swift,
+  mysql,
+  sqlite,
+  html,
+  css,
+  js,
+  react,
+  typesecript,
+  tailwind,
+  next,
+  figma,
+  firebase,
+  sasc,
+  binus,
+  tfisc,
+  ada,
+  youthmatters,
+  aidbuddy,
+  tapesync,
+  picverse,
+  dojomovies,
+  thalafish,
+  planaway,
+  github,
+  github_white,
+  github_purple,
+  linkedin_purple,
+  instagram_purple,
+  github_lp,
+  linkedin_lp,
+  instagram_lp,
 };
 
 export const techStack = [
-    assets.C, 
-    assets.python, 
-    assets.dart, 
-    assets.flutter, 
-    assets.kotlin, 
-    assets.swift, 
-    assets.mysql, 
-    assets.sqlite,
-    assets.html, 
-    assets.css, 
-    assets.js, 
-    assets.react, 
-    assets.typesecript, 
-    assets.tailwind, 
-    assets.next
+  assets.C,
+  assets.python,
+  assets.dart,
+  assets.flutter,
+  assets.kotlin,
+  assets.swift,
+  assets.mysql,
+  assets.sqlite,
+  assets.html,
+  assets.css,
+  assets.js,
+  assets.react,
+  assets.typesecript,
+  assets.tailwind,
+  assets.next,
+  assets.firebase,
 ];
 
 export const experienceData = [
-    { 
-        icon: assets.sasc, 
-        title: 'SASC Senior Mentor', 
-        subtitle: 'SASC BINUS University',
-    },
-    { 
-        icon: assets.binus, 
-        title: 'Freshmen Partner', 
-        subtitle: 'BINUS University',
-    },
-    { 
-        icon: assets.binus, 
-        title: 'Freshmen Leader', 
-        subtitle: 'BINUS University',
-    },
-    { 
-        icon: assets.tfisc, 
-        title: 'Activist of CP : Education', 
-        subtitle: 'TFI Student Community',
-    },
-    { 
-        icon: assets.ada, 
-        title: 'iOS Foundation Cohort 19', 
-        subtitle: 'Apple Developer Academy @BINUS',
-    },
-    { 
-        icon: assets.youthmatters, 
-        title: 'Graphic Designer', 
-        subtitle: 'Youth Matters',
-    },
-]
+  {
+    icon: assets.binus,
+    title: "Author & Presenter at ICOBAR-SMART 2026",
+    subtitle: "BINUS University",
+  },
+  {
+    icon: assets.sasc,
+    title: "SASC Senior Mentor",
+    subtitle: "SASC BINUS University",
+  },
+  {
+    icon: assets.binus,
+    title: "Freshmen Partner",
+    subtitle: "BINUS University",
+  },
+  {
+    icon: assets.binus,
+    title: "Freshmen Leader",
+    subtitle: "BINUS University",
+  },
+  {
+    icon: assets.tfisc,
+    title: "Activist of CP : Education",
+    subtitle: "TFI Student Community",
+  },
+  {
+    icon: assets.ada,
+    title: "iOS Foundation Cohort 19",
+    subtitle: "Apple Developer Academy @BINUS",
+  },
+  {
+    icon: assets.youthmatters,
+    title: "Graphic Designer",
+    subtitle: "Youth Matters",
+  },
+];
 
 export const projectData = [
-    {
-        title: 'AidBuddy',
-        details: ['Group Project', 'Application', '2023'],
-        description: 'AidBuddy is a vital mobile app developed using Swift, designed to provide immediate and reliable first aid information in emergency situations. Whether dealing with a minor injury or a critical medical emergency, AidBuddy offers step-by-step guidance to help users respond quickly and effectively before professional assistance arrives.',
-        bgImage: assets.aidbuddy,
-        icons: [assets.figma, assets.swift],
-        link: 'https://github.com/olynsn15/AidBuddy',
-    },
-    {
-        title: 'TapeSync',
-        details: ['Group Project', 'Application', '2024'],
-        description: 'TapeSync is a mobile app built with Flutter that serves as a digital video tape store, allowing customers to browse and purchase tapes, while admins manage tape data. Developed with a Node.js back-end and MySQL database, the app supports CRUD operations, secure JWT-based authentication, and features a responsive UI for a smooth user experience.',
-        bgImage: assets.tapesync,
-        icons: [assets.dart, assets.flutter, assets.mysql],
-        link: 'https://github.com/olynsn15/MCS-DoJo-Movie',
-    },
-    {
-        title: 'Picverse',
-        details: ['Individual Project', 'Website', '2025'],
-        description: 'Picverse Community is a dynamic online platform designed to empower digital artists through sharing, connection, and recognition. The website features five primary pages, initially prototyped in Figma and then developed using HTML, CSS, and JavaScript, ensuring a responsive, smooth and engaging user experience across various devices.',
-        bgImage: assets.picverse,
-        icons: [assets.figma, assets.html, assets.css, assets.js],
-        link: 'https://github.com/olynsn15/Picverse',
-    },
-    {
-        title: 'DoJo Movies',
-        details: ['Group Project', 'Application', '2025'],
-        description: 'DoJo Movie is an Android mobile app that lets users browse and purchase high-quality films while also locating nearby DoJo Movie stores via Google Maps. Built with SQLite for data storage, the app features user authentication, OTP verification, film browsing with data fetched via Volley, and a Google Maps-integrated home page.',
-        bgImage: assets.dojomovies,
-        icons: [assets.kotlin, assets.sqlite],
-        link: 'https://github.com/olynsn15/MCS-DoJo-Movie',
-    },
-]
+  {
+    title: "AidBuddy",
+    details: ["Group Project", "Application", "2023"],
+    description:
+      "AidBuddy is a vital mobile app developed using Swift, designed to provide immediate and reliable first aid information in emergency situations. Whether dealing with a minor injury or a critical medical emergency, AidBuddy offers step-by-step guidance to help users respond quickly and effectively before professional assistance arrives.",
+    bgImage: assets.aidbuddy,
+    icons: [assets.figma, assets.swift],
+    link: "https://github.com/olynsn15/AidBuddy",
+  },
+  {
+    title: "TapeSync",
+    details: ["Group Project", "Application", "2024"],
+    description:
+      "TapeSync is a mobile app built with Flutter that serves as a digital video tape store, allowing customers to browse and purchase tapes, while admins manage tape data. Developed with a Node.js back-end and MySQL database, the app supports CRUD operations, secure JWT-based authentication, and features a responsive UI for a smooth user experience.",
+    bgImage: assets.tapesync,
+    icons: [assets.dart, assets.flutter, assets.mysql],
+    link: "https://github.com/olynsn15/MCS-DoJo-Movie",
+  },
+  {
+    title: "Picverse",
+    details: ["Individual Project", "Website", "2025"],
+    description:
+      "Picverse Community is a dynamic online platform designed to empower digital artists through sharing, connection, and recognition. The website features five primary pages, initially prototyped in Figma and then developed using HTML, CSS, and JavaScript, ensuring a responsive, smooth and engaging user experience across various devices.",
+    bgImage: assets.picverse,
+    icons: [assets.figma, assets.html, assets.css, assets.js],
+    link: "https://github.com/olynsn15/Picverse",
+  },
+  {
+    title: "DoJo Movies",
+    details: ["Group Project", "Application", "2025"],
+    description:
+      "DoJo Movie is an Android mobile app that lets users browse and purchase high-quality films while also locating nearby DoJo Movie stores via Google Maps. Built with SQLite for data storage, the app features user authentication, OTP verification, film browsing with data fetched via Volley, and a Google Maps-integrated home page.",
+    bgImage: assets.dojomovies,
+    icons: [assets.kotlin, assets.sqlite],
+    link: "https://github.com/olynsn15/MCS-DoJo-Movie",
+  },
+  {
+    title: "ThalaFish",
+    details: ["Group Project", "Application", "2026"],
+    description:
+      "ThalaFish is an iOS-based marketplace application developed to facilitate the buying and selling of fish through a seamless and user-friendly mobile experience. The application supports two user roles: Admin and Buyer. The application was developed using Swift (iOS) with Core Data as the local database for efficient on-device data management.",
+    bgImage: assets.thalafish,
+    icons: [assets.swift],
+    link: "",
+  },
+  {
+    title: "PlanAway",
+    details: ["Group Project", "Application", "2026"],
+    description:
+      "PlanAway is a Travel Bucket List application built using Swift designed to help users explore, save, and organize their dream travel destinations in a practical and structured way. This project was developed using Swift and SwiftUI for the iOS frontend, with Firebase as the backend for authentication and data management.",
+    bgImage: assets.planaway,
+    icons: [assets.swift, assets.firebase],
+    link: "https://github.com/olynsn15/PlanAway",
+  },
+];
 
 export const contactData = [
-    { 
-        icon: assets.linkedin_purple, 
-        darkIcon: assets.linkedin_lp,
-        name: 'Carolyn Santana', 
-        link: 'https://www.linkedin.com/in/carolyn-santana15/',
-    },
-    { 
-        icon: assets.github_purple, 
-        darkIcon: assets.github_lp,
-        name: 'olynsn15', 
-        link: 'https://github.com/olynsn15',
-    },
-    { 
-        icon: assets.instagram_purple, 
-        darkIcon: assets.instagram_lp,
-        name: '@olyn.sna', 
-        link: 'https://www.instagram.com/olyn.sna',
-    },
-]
+  {
+    icon: assets.linkedin_purple,
+    darkIcon: assets.linkedin_lp,
+    name: "Carolyn Santana",
+    link: "https://www.linkedin.com/in/carolyn-santana15/",
+  },
+  {
+    icon: assets.github_purple,
+    darkIcon: assets.github_lp,
+    name: "olynsn15",
+    link: "https://github.com/olynsn15",
+  },
+  {
+    icon: assets.instagram_purple,
+    darkIcon: assets.instagram_lp,
+    name: "@olyn.sna",
+    link: "https://www.instagram.com/olyn.sna",
+  },
+];
